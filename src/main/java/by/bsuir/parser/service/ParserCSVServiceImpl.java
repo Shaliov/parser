c23fcb34a18e4b310c5c82ad6e2483698fe0a744
Shaliov <Andrey.Shaliov@gmail.com>
2018-03-02 18:50:46 +0300

    Добавление сервиса

---
+++ src/main/java/by/bsuir/parser/service/ParserCSVServiceImpl.java
@@ -0,0 +1,11 @@
+package by.bsuir.parser.service;
+
+/**
+ * @author Shaliou_AG
+ */
+public class ParserCSVServiceImpl implements ParserService {
+    @Override
+    public void parse() {
+
+    }
+}
---
+++ src/main/java/by/bsuir/parser/service/ParserService.java
@@ -0,0 +1,8 @@
+package by.bsuir.parser.service;
+
+/**
+ * @author Shaliou_AG
+ */
+public interface ParserService {
+    void parse();
+}
